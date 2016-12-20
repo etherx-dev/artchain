bf98db84194b856909c6b8b5a8d27372c85ce443
etherx-dev <etherxdev@gmail.com>
2016-12-20 01:32:28 +0000

    Update contract.sol

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -4,16 +4,13 @@ contract token { mapping (address => uint256) public balanceOf;  }
 
 contract owned {
     address public owner;
-
     function owned() {
         owner = msg.sender;
     }
-
     modifier onlyOwner {
         if (msg.sender != owner) throw;
         _;
     }
-
     function transferOwnership(address newOwner) onlyOwner {
         owner = newOwner;
     }
@@ -24,12 +21,10 @@ contract Token {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
 }
 
-
-contract Association is owned, tokenRecipient {
+contract Artwork is owned, tokenRecipient {
 /* Below this is still to be modified  */
     /* Contract Variables and events */
     uint public minimumQuorum;
-    uint public debatingPeriodInMinutes;
     Proposal[] public proposals;
     uint public numProposals;
     token public sharesTokenAddress;
@@ -43,7 +38,6 @@ contract Association is owned, tokenRecipient {
         address recipient;
         uint amount;
         string description;
-        uint votingDeadline;
         bool executed;
         bool proposalPassed;
         uint numberOfVotes;
@@ -57,118 +51,4 @@ contract Association is owned, tokenRecipient {
         address voter;
     }
 
-    /* modifier that allows only shareholders to vote and create new proposals */
-    modifier onlyShareholders {
-        if (sharesTokenAddress.balanceOf(msg.sender) == 0) throw;
-        _;
-    }
-
-    /* First time setup */
-    function Association(token sharesAddress, uint minimumSharesToPassAVote, uint minutesForDebate) payable {
-        Appraisal(sharesAddress, minimumSharesToPassAVote, minutesForDebate);
-    }
-
-    /*change rules*/
-    function Appraisal(token sharesAddress, uint minimumSharesToPassAVote, uint minutesForDebate) onlyOwner {
-        sharesTokenAddress = token(sharesAddress);
-        if (minimumSharesToPassAVote == 0 ) minimumSharesToPassAVote = 1;
-        minimumQuorum = minimumSharesToPassAVote;
-        debatingPeriodInMinutes = minutesForDebate;
-        ChangeOfRules(minimumQuorum, debatingPeriodInMinutes, sharesTokenAddress);
-    }
-
-    /* Function to register a new Artwork */
-    function newArtist(
-        address beneficiary,
-        uint etherAmount,
-        string JobDescription,
-        bytes transactionBytecode
-    )
-        onlyShareholders
-        returns (uint proposalID)
-    {
-        proposalID = proposals.length++;
-        Proposal p = proposals[proposalID];
-        p.recipient = beneficiary;
-        p.amount = etherAmount;
-        p.description = JobDescription;
-        p.proposalHash = sha3(beneficiary, etherAmount, transactionBytecode);
-        p.votingDeadline = now + debatingPeriodInMinutes * 1 minutes;
-        p.executed = false;
-        p.proposalPassed = false;
-        p.numberOfVotes = 0;
-        ProposalAdded(proposalID, beneficiary, etherAmount, JobDescription);
-        numProposals = proposalID+1;
-    }
-
-    /* function to check if a proposal code matches */
-    function checkProposalCode(
-        uint proposalNumber,
-        address beneficiary,
-        uint etherAmount,
-        bytes transactionBytecode
-    )
-        constant
-        returns (bool codeChecksOut)
-    {
-        Proposal p = proposals[proposalNumber];
-        return p.proposalHash == sha3(beneficiary, etherAmount, transactionBytecode);
-    }
-
-    /* */
-    function vote(uint proposalNumber, bool supportsProposal)
-        onlyShareholders
-        returns (uint voteID)
-    {
-        Proposal p = proposals[proposalNumber];
-        if (p.voted[msg.sender] == true) throw;
-
-        voteID = p.votes.length++;
-        p.votes[voteID] = Vote({inSupport: supportsProposal, voter: msg.sender});
-        p.voted[msg.sender] = true;
-        p.numberOfVotes = voteID +1;
-        Voted(proposalNumber,  supportsProposal, msg.sender);
-    }
-
-    function executeProposal(uint proposalNumber, bytes transactionBytecode) returns (int result) {
-        Proposal p = proposals[proposalNumber];
-        /* Check if the proposal can be executed */
-        if (now < p.votingDeadline  /* has the voting deadline arrived? */
-            ||  p.executed        /* has it been already executed? */
-            ||  p.proposalHash != sha3(p.recipient, p.amount, transactionBytecode)) /* Does the transaction code match the proposal? */
-            throw;
-
-        /* tally the votes */
-        uint quorum = 0;
-        uint yea = 0;
-        uint nay = 0;
-
-        for (uint i = 0; i <  p.votes.length; ++i) {
-            Vote v = p.votes[i];
-            uint voteWeight = sharesTokenAddress.balanceOf(v.voter);
-            quorum += voteWeight;
-            if (v.inSupport) {
-                yea += voteWeight;
-            } else {
-                nay += voteWeight;
-            }
-        }
-
-        /* execute result */
-        if (quorum <= minimumQuorum) {
-            /* Not enough significant voters */
-            throw;
-        } else if (yea > nay ) {
-            /* has quorum and was approved */
-            p.executed = true;
-            if (!p.recipient.call.value(p.amount * 1 ether)(transactionBytecode)) {
-                throw;
-            }
-            p.proposalPassed = true;
-        } else {
-            p.proposalPassed = false;
-        }
-        // Fire Events
-        ProposalTallied(proposalNumber, result, quorum, p.proposalPassed);
-    }
 }
