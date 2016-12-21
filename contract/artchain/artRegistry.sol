09d55d81251e2e5ba73607b45c6ed2b13792dbda
etherx-dev <etherxdev@gmail.com>
2016-12-21 20:13:05 +0000

    Create artRegistry.sol

---
+++ contract/artchain/artRegistry.sol
@@ -0,0 +1,16 @@
+contract artRegistry {
+
+// TODO: map validator address and more...
+
+/**
+ * @title artRegistry
+ * @author Artchain.org
+ * @notice Registers artwork on the ethereum blockchain, allowing users to appraise.
+ * @dev This contract is still under development.
+ */
+    struct Appraiser {
+        string name;
+        string review;
+    }
+
+}
