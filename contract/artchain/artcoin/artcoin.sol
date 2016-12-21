b3f05df470434219374da7f246973774d0eef1e4
artchain <laurenzo@artchain.org>
2016-12-21 11:05:57 +0000

    Update artcoin.sol

--- contract/artchain/artcoin/artcoin.sol
+++ contract/artchain/artcoin/artcoin.sol
@@ -1,1 +1,42 @@
+pragma solidity ^0.4.4;
 
+
+import "./StandardToken.sol";
+
+
+/*
+ * Artcoin
+ *
+ * 
+ */
+contract CrowdsaleToken is StandardToken {
+  
+  // name of the artcoin ERC20 token
+  string public name = "artcoin";
+  // symbol
+  string public symbol = "ART";
+  // smallest denomination of artcoin is 0.00000001 Laurenzo.
+  uint public decimals = 8;
+
+  // 1 ether = 10 artcoin. 1:1 Exchange of art.token for artcoin TBD.
+  uint PRICE = 10;
+  
+  // issue artcoin to the sender of ethereum to this contract.
+  function () payable {
+    createTokens(msg.sender);
+  }
+  
+  function createTokens(address recipient) payable {
+    if (msg.value == 0) throw;
+
+    uint tokens = safeMul(msg.value, getPrice());
+
+    totalSupply = safeAdd(totalSupply, tokens);
+    balances[recipient] = safeAdd(balances[recipient], tokens);
+  }
+  
+  // get price of artcoin. 
+  function getPrice() constant returns (uint result){
+    return PRICE;
+  }
+}
