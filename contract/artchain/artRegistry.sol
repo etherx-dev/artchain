3162844ceed2d3d31793d7494e140c44a4fedafb
etherx-dev <etherxdev@gmail.com>
2016-12-21 20:22:51 +0000

    Update artRegistry.sol
    fix bugs

--- contract/artchain/artRegistry.sol
+++ contract/artchain/artRegistry.sol
@@ -1,42 +1,39 @@
 contract artRegistry {
 
-// TODO: map validator address and more...
-
-/**
- * @title artRegistry
- * @author Artchain.org
- * @notice Registers artwork on the ethereum blockchain, allowing users to appraise.
- * @dev This contract is still under development.
- */
+  // TODO: map validator address and more...
+  
+  /**
+   * @title artRegistry
+   * @author Artchain.org
+   * @notice Registers artwork on the ethereum blockchain, allowing users to appraise.
+   * @dev This contract is still under development.
+   */
  
-    enum existence { yes, no } // does the artwork exist
-    struct Artwork {
-        exists existence; 
-        string artistName; // name of artist
-        string titleofwork, // title of artwork
-        uint dateCreated; // dateCreated in timestamp
-        uint issueDate; // issueDate in timestamp
-        Appraiser[] appraisers;
-    }
-    
-    struct Appraiser {
-        string name;
-        string review;
-    }
-    
-    function artDetails(uint artId) constant returns(
-      exists existence, 
-      string titleofwork, 
-      string artistname, 
-      uint datecreated, 
-      uint issuedate) {
+  struct Artwork {
+      string artistName; // name of artist
+      string titleofwork; // title of artwork
+      uint dateCreated; // dateCreated in timestamp
+      uint issueDate; // issueDate in timestamp
+      Appraiser[] appraisers;
+  }
+  Artwork[] artworks; // will improve later
 
-        return (
-            artworks[artId].exists,
-            artworks[artId].titleofwork,
-            artworks[artId].artistname,
-            artworks[artId].datecreated,
-            artworks[artId].issuedate
-        );
-    }
+  struct Appraiser {
+      string name;
+      string review;
+  }
+  
+  function artDetails(uint artId) constant returns(
+    string titleofwork, 
+    string artistname, 
+    uint datecreated, 
+    uint issuedate) {
+
+      return (
+          artworks[artId].titleofwork,
+          artworks[artId].artistName,
+          artworks[artId].dateCreated,
+          artworks[artId].issueDate
+      );
+  }
 }
