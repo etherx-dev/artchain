8b2f2c81765e74c9115491e1825a7d8bd451e1c8
etherx-dev <etherxdev@gmail.com>
2016-12-22 02:07:44 +0000

    Update artRegistry.sol

--- contract/artchain/artRegistry.sol
+++ contract/artchain/artRegistry.sol
@@ -7,6 +7,7 @@ contract artRegistry {
   * @notice Registers artwork on the ethereum blockchain, allowing users to appraise.
   * @dev This contract is still under development.
   */
+  event RegisteredAppraisal(uint artId, uint apprId);
   event RegisteredArt(uint artId);
 
   struct Artwork {
@@ -14,17 +15,18 @@ contract artRegistry {
       string titleofwork; // title of artwork
       uint dateCreated; // dateCreated in timestamp
       uint issueDate; // issueDate in timestamp
-      Appraiser[] appraisers;
+      Appraiser[] appraisers; // array of apraisers
   }
   Artwork[] artworks; // will improve later
 
   struct Appraiser {
-      string name;
-      string review;
+      string name; // name of appraiser
+      string review; // review from the appraiser
   }
-  // returns total number of artworks on registry
-  function totalArts() constant returns (uint res) {
-    return artworks.length;
+
+  // @param artId  returns 
+  function appraisalsOf(uint artId) constant returns(uint res) {
+      return artworks[artId].appraisers.length;
   }
 
   function artDetails(uint artId) constant returns(
@@ -41,18 +43,37 @@ contract artRegistry {
       );
   }
   
+  function appraiseArtwork(uint artId, string name,string review) returns(uint apprId) {
+      if(bytes(name).length != 0 || bytes(review).length != 0) {
+        apprId = artworks[artId].appraisers.length++; // assign ID for the appraisal
+        artworks[artId].appraisers[apprId].name = name; 
+        artworks[artId].appraisers[apprId].review = review;
+        RegisteredAppraisal(artId, apprId);
+      } else {
+        throw;
+      }
+  }
+
+  
   function publishArtwork(string titleofwork, string artistName, uint dateCreated) returns(uint artId) {
     
     if(bytes(titleofwork).length == 0 || bytes(artistName).length == 0) {
-        throw;
+      artId = artworks.length++;
+      artworks[artId].titleofwork = titleofwork;
+      artworks[artId].artistName = artistName;
+      artworks[artId].dateCreated = dateCreated;
+      artworks[artId].issueDate = block.timestamp;
+      RegisteredArt(artId);
+    } else {
+      throw;
     }
     
-    artId = artworks.length++;
-    artworks[artId].titleofwork = titleofwork;
-    artworks[artId].artistName = artistName;
-    artworks[artId].dateCreated = dateCreated;
-    artworks[artId].issueDate = block.timestamp;
 
-    RegisteredArt(artId);
   }
+  
+  // returns total number of artworks on registry
+  function totalArts() constant returns (uint res) {
+    return artworks.length;
+  }
+  
 }
