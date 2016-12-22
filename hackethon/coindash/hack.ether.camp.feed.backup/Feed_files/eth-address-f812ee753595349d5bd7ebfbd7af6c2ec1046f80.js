5967dfae80718d2b5c6d0aed872df51597f01810
etherx-dev <etherxdev@gmail.com>
2016-12-22 22:10:53 +0000

    backup of feed, all voters of hack.ether.camp hackethon
    backup of feed, all voters of hack.ether.camp hackethon

---
+++ hackethon/coindash/hack.ether.camp.feed.backup/Feed_files/eth-address-f812ee753595349d5bd7ebfbd7af6c2ec1046f80.js
@@ -0,0 +1,85 @@
+'use strict';
+
+angular.module('app.blockchain').factory('EthereumAddress', ['$q',
+  function($q) {
+
+    var EthUtil = window.modules.EthUtil;
+
+    var jobsCnt = 0;
+
+    var getPKeyIter = function(pKey, iterations) {
+
+      var deferred = $q.defer();
+
+      setTimeout(function() {
+        for (var i = 0; i < iterations; i++) {
+          var binary = EthUtil.sha3(pKey.value);
+          pKey.value = EthUtil.bufferToHex(binary)
+        }
+        deferred.resolve();
+      }, 0);
+
+      return deferred.promise;
+    }
+
+    return {
+
+      prefixed: function(address) {
+        return address.startsWith('0x') ? address : '0x' + address;
+      },
+
+      notPrefixed: function(address) {
+        return address.startsWith('0x') ? address.substring(2, address.length) : address;
+      },
+
+      isValid: function(value) {
+        return /^(0x)?[0-9a-fA-F]{40}$/.test(value);
+      },
+
+      generatePKey: function(seed) {
+
+        var deferred = $q.defer();
+
+        // return if seed is a PK itself
+        if (/^(0x)?[0-9a-fA-F]{64}$/.test(seed)) {
+          deferred.resolve(this.prefixed(seed));
+          return deferred.promise;
+        }
+
+        var pKey = { value: seed }
+        var jobId = ++jobsCnt;
+
+        // otherwise do sha3 N times
+        
+        getPKeyIter(pKey)
+          .then(function() {
+            if (jobId != jobsCnt) return false;
+            return getPKeyIter(pKey, 1000);
+          })
+          .then(function() {
+            if (jobId != jobsCnt) return false;
+            return getPKeyIter(pKey, 1031);
+          })
+          .then(function(ret) {
+
+            if (ret == false) {
+              deferred.reject();
+              return undefined;
+            }
+
+            deferred.resolve(pKey.value);
+          });
+
+        return deferred.promise;
+      },
+
+      getFromPKey: function(pKey) {
+        // sanity check
+        if (pKey.length != 66) return undefined;
+        return EthUtil.bufferToHex(EthUtil.privateToAddress(pKey));
+      }
+
+    }
+
+  }
+]);
